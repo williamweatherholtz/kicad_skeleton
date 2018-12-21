43bfcc4c81ccd4fb4eae6a79d7c7b1dbb0ec62d4
Weatherholtz, William (W) <WWeatherholtz@dow.com>
2018-12-21 14:02:35 -0500

    initialization

--- boot.bat
+++ boot.bat
@@ -1,6 +1,6 @@
 set arg1=%1
 git submodule init
-git submodule --update --merge
+git submodule --update --init --recursive --merge
 git remote set-url origin %1
 git add -A
 git commit -m "initialization"
