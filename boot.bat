3f1310964d6123131b1dc64473b536520ed901fc
Weatherholtz, William (W) <WWeatherholtz@dow.com>
2018-12-21 14:03:13 -0500

    fixed batch

--- boot.bat
+++ boot.bat
@@ -1,6 +1,5 @@
 set arg1=%1
-git submodule init
-git submodule --update --init --recursive --merge
+git submodule update --init --recursive --merge
 git remote set-url origin %1
 git add -A
 git commit -m "initialization"
