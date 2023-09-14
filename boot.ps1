9e5824dc65d7074b6315d738564a83e187978e50
William Weatherholtz <williamweatherholtz@gmail.com>
2023-09-14 15:35:21 -0400

    updated sub

--- boot.ps1
+++ boot.ps1
@@ -1,9 +1,5 @@
-echo off
-set arg1=%1
 git submodule update --init --recursive --merge
-git pull --allow-unrelated-histories
-git add -A
-git commit -m "initialization"
-git push
-
+cd .\subs
+git pull origin master
+cd ..
 echo "initialization complete"
